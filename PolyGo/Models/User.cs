21ffb2f3f65a11c2cc6c3409b11e9c4863fec45d
yaroslaw-shchurikhin <shurihin.yaroslav@yandex.ru>
2022-02-10 13:51:22 +0300

    Add basic pages and add they to shell

---
+++ PolyGo/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolyGo.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string GroupNum { get; set; }
+        public bool IsStudent { get; set; }
+    }
+}
+
