7ed717281a5b8fdde50e6168edb34ec03fc4c4a3
Roman-Shik <111romashka@gmail.com>
2022-02-28 03:51:16 +0300

    Add graph class

---
+++ PolyGo/Models/Map/Graph.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+
+namespace PolyGo.Models.Map
+{
+  public class Graph
+  {
+    private List<Edge> Edges;
+    private List<int> nodeIds;
+    private int nodeCount;
+
+    /// <summary>
+    /// Create graph out of nodes
+    /// </summary>
+    /// <param name="nodes">Nodes of graph</param>
+    public Graph(List<Node> nodes)
+    {
+      Edges = new List<Edge>();
+      nodeIds = new List<int>();
+      nodeCount = nodes.Count;
+      foreach (var node in nodes)
+      {
+        nodeIds.Add(node.Id);
+        foreach (var adjacentNode in node.adjacentNodes)
+        {
+          Edges.Add(new Edge(node.Id, adjacentNode.Item1, adjacentNode.Item2));
+        }
+      }
+    }
+
+    /// <summary>
+    /// Find the shortest path from node with startId to node with endId.
+    /// </summary>
+    /// <param name="startId">Id of start node in path</param>
+    /// <param name="endId">Id of end node in path</param>
+    /// <returns>Path as list of nodes. If path doesn't exist returns empty list</returns>
+    public List<int> findPath(int startId, int endId)
+    {
+      // pathWeight[v] - weight of shortest path from start to any vertex v
+      Dictionary<int, float> pathWeight = new Dictionary<int, float>();
+      // parent[v] - predecessor of vertex v in shortest path
+      Dictionary<int, int> parent = new Dictionary<int, int>();
+      // is paths changed
+      bool changed = true;
+
+      // Initialize containers
+      foreach (var nodeId in nodeIds)
+      {
+        pathWeight[nodeId] = System.Single.MaxValue;
+        parent[nodeId] = -1;
+      }
+      pathWeight[startId] = 0.0f;
+
+      // Find shortest paths
+      for (int i = 0; i < nodeCount && changed; ++i)
+      {
+        changed = false;
+        foreach (var edge in Edges)
+        {
+          if (pathWeight[edge.StartNodeId] != System.Single.MaxValue &&
+              pathWeight[edge.EndNodeId] > pathWeight[edge.StartNodeId] + edge.Weight)
+          {
+            pathWeight[edge.EndNodeId] = pathWeight[edge.StartNodeId] + edge.Weight;
+            parent[edge.EndNodeId] = edge.StartNodeId;
+            changed = true;
+          }
+        }
+      }
+
+      // Is path exist ?
+      if (parent[endId] == -1)
+      {
+        return new List<int>();
+      }
+
+      // Save every node of path
+      List<int> path = new List<int>();
+      path.Add(endId);
+      int curNodeId = endId;
+      while (parent[curNodeId] != -1)
+      {
+        curNodeId = parent[curNodeId];
+        path.Add(curNodeId);
+      }
+      path.Reverse();
+
+      return path;
+    }
+  }
+}
---
+++ PolyGo/Models/Map/Node.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+namespace PolyGo.Models.Map
+{
+  /// <summary>
+  /// This class represents node of graph and also contains specific info about node 
+  /// </summary>
+  public class Node
+  {
+    /// <summary>
+    /// Every node must have unique Id for correct work
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Every adjacent node seems as tuple of
+    /// adjacent node id and weight of edge to this node
+    /// </summary>
+    public List<(int, float)> adjacentNodes { get; set; }
+
+    /// <summary>
+    /// Contains all specific info about node
+    /// </summary>
+    public NodeInfo Info { get; set; }
+
+    /// <summary>
+    /// Creates node of graph
+    /// </summary>
+    /// <param name="id">unique id of node</param>
+    /// <param name="adcNodes">list of adjacent nodes. 
+    /// Every adjacent node seems as tuple of
+    /// adjacent node id and weight of edge to this node.
+    /// Wieght of edge must be positive</param>
+    /// <param name="info">all specific info about node</param>
+    public Node(int id, List<(int, float)> adcNodes, NodeInfo info)
+    {
+      Id = id;
+      adjacentNodes = new List<(int, float)>(adcNodes);
+      Info = new NodeInfo(info);
+    }
+  }
+
+  /// <summary>
+  /// This class contains all info about node as classroom number and etc
+  /// </summary>
+  public class NodeInfo
+  {
+    public NodeInfo()
+    {
+    }
+    public NodeInfo(NodeInfo other)
+    {
+    }
+  }
+}
