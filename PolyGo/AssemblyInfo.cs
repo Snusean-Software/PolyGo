79939b63341dafd396530508e8e3864a6508359d
yaroslaw-shchurikhin <shurihin.yaroslav@yandex.ru>
2022-03-04 11:05:02 +0300

    Fix AssemblyInfo.cs

--- PolyGo/AssemblyInfo.cs
+++ PolyGo/AssemblyInfo.cs
@@ -1,7 +1,5 @@
 using Xamarin.Forms.Xaml;
 using System.Resources;
-using Android.App;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: NeutralResourcesLanguage("ru-RU")]
-[assembly: UsesPermission(Android.Manifest.Permission.AccessNetworkState)] 
