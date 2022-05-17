9420edefb09cdac48c4c73eac39edd30010eb426
SNK-LT <litvinov0927@yandex.ru>
2022-05-17 19:09:39 +0300

    Add icons on tab bar

--- PolyGo/AssemblyInfo.cs
+++ PolyGo/AssemblyInfo.cs
@@ -1,5 +1,8 @@
 using Xamarin.Forms.Xaml;
 using System.Resources;
+using Xamarin.Forms;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: NeutralResourcesLanguage("ru-RU")]
+
+[assembly: ExportFont("FA_Solid_900.otf", Alias ="AW")] 
