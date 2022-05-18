27b8d7935235591b33f030ee7d674f3345854925
SNK-LT <litvinov0927@yandex.ru>
2022-05-18 10:09:12 +0300

    my local commit

--- PolyGo/AssemblyInfo.cs
+++ PolyGo/AssemblyInfo.cs
@@ -5,4 +5,9 @@ using Xamarin.Forms;
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: NeutralResourcesLanguage("ru-RU")]
 
-[assembly: ExportFont("FA_Solid_900.otf", Alias ="AW")] 
+[assembly: ExportFont("FA_Solid_900.otf", Alias ="AW")]
+[assembly: ExportFont("Montserrat_Regular.ttf", Alias = "MonReg")]
+[assembly: ExportFont("Montserrat_Bold.ttf", Alias = "MonBold")]
+[assembly: ExportFont("Montserrat_Bold.ttf", Alias = "MonBlack")]
+[assembly: ExportFont("Roboto_Thin.ttf", Alias = "RobThin")]
+[assembly: ExportFont("Roboto_Regular.ttf", Alias = "RobReg")] 
---
+++ PolyGo/Views/InitialSetup/GroupNumberPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PolyGo.Views.InitialSetup
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GroupNumberPage : ContentPage
+	{
+		public GroupNumberPage()
+		{
+			InitializeComponent();
+			content_page.BackgroundImageSource = ImageSource.FromResource("PolyGo.Resources.SetUp.second_background_image.png", GetType().Assembly);
+
+		}
+	}
+}
